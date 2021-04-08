2fdeb2332ac50d7580b4ccbc16846fbce9228c2d
Leonardo Delgado <sdelgadom@unal.edu.co>
2021-04-08 17:28:27 -0500

    corrección librerias maven

--- src/main/java/com/uroom/backend/Repository/UserRepository.java
+++ src/main/java/com/uroom/backend/Repository/UserRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.repository.CrudRepository;
 public interface UserRepository extends CrudRepository<User, Integer> {
     @Query(value = "SELECT * FROM user where cellphone like  ?1 ;", nativeQuery = true)
     Iterable<User> findByCellphone(String cellphone);
+
 }
