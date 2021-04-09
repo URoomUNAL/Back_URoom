33438e5aa336c6ace3a1776389f52a9187ab853f
Ricardo Andrés Calvo <rcalvom@unal.edu.co>
2021-04-09 14:22:44 -0500

    Commit inicial.

---
+++ src/main/java/com/uroom/backend/UroomBackApplication.java
@@ -0,0 +1,13 @@
+package com.uroom.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UroomBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UroomBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uroom/backend/UroomBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uroom.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UroomBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
